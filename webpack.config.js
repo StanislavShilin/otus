f724248603b9384e65778f464d106e504938c550
shilin <s.shilin@samoletgroup.ru>
2020-04-05 11:41:20 +0300

    настроил проект

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const path = require("path");
+
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const {CleanWebpackPlugin} = require("clean-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.tsx",
+  devtool: "source-map",
+  resolve: {
+    extensions: [".js", ".jsx", ".ts", ".tsx", ".json"]
+  },
+  output: {
+    path: path.join(__dirname, "/dist"),
+    filename: '[name].[contenthash].js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|ts)x?$/,
+        loader: "babel-loader",
+        exclude: /node_modules/
+      }
+    ]
+  },
+  devServer: {
+    historyApiFallback: true
+  },
+  plugins: [
+      new HtmlWebpackPlugin({
+      template: "./src/index.html"
+      }),
+      new CleanWebpackPlugin()
+  ]
+};
